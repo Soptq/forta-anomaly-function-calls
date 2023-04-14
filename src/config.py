917f28e645af640d8a2dad01e758921df71257c2
Soptq <32592090+soptq@users.noreply.github.com>
2023-04-14 11:50:09 +0800

    raise threshold to 0.85

--- src/config.py
+++ src/config.py
@@ -1,5 +1,5 @@
 HISTORY_TTL = 60 * 60 * 24 * 7  # 7 days
 MIN_RECORDS_TO_DETECT = 2048  # there must be at least 100 function call records to detect a possible anomaly.
 MIN_TIME_TO_COLLECT_NS = 30 * 60 * 10 ** 9  # 30 minutes
-ANOMALY_THRESHOLD = 0.8  # the threshold to determine whether a function call is an anomaly.
+ANOMALY_THRESHOLD = 0.85  # the threshold to determine whether a function call is an anomaly.
 NOISE_SCALAR = 1e-10
