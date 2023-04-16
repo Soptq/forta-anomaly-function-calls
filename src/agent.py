b57a6a63be26a853da95156f3cde4dbe49feaffa
Soptq <32592090+soptq@users.noreply.github.com>
2023-04-16 13:48:16 +0800

    bump version

--- src/agent.py
+++ src/agent.py
@@ -24,8 +24,7 @@ def parse_traces(transaction_event: TransactionEvent):
     if len(transaction_event.traces) > 0:
         # get all traces with `call` type
         for trace in transaction_event.traces:
-            print(f"'{trace.error}'", type(trace.error), trace.error)
-            if trace.error is not None or trace.error == "":
+            if trace.error is not None or trace.error != "":
                 continue
             # deal with suicided contract
             if trace.type.lower() == 'suicide':
